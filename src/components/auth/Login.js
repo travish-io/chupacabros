a6e530f022843fda6440f443adc207347a5cda26
Travis Hall <t.hall89d@gmail.com>
2021-09-14 15:36:51 -0400

    copied login stuff from heoney rae

---
+++ src/components/auth/Login.js
@@ -0,0 +1,80 @@
+import React, { useRef, useState } from "react";
+import { Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
+import "./Login.css";
+
+export const Login = () => {
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+  const existDialog = useRef();
+  const history = useHistory();
+
+  const existingUserCheck = () => {
+    return fetch(`http://localhost:8088/users?email=${user}`)
+      .then((res) => res.json())
+      .then((user) => (user.length ? user[0] : false));
+  };
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    existingUserCheck().then((exists) => {
+      if (exists) {
+        localStorage.setItem("chupacabro_user", exists.id);
+        history.push("/");
+      } else {
+        existDialog.current.showModal();
+      }
+    });
+  };
+
+  return (
+    <main className="container--login">
+      <dialog className="dialog dialog--auth" ref={existDialog}>
+        <div>User does not exist</div>
+        <button
+          className="button--close"
+          onClick={(e) => existDialog.current.close()}
+        >
+          Close
+        </button>
+      </dialog>
+
+      <section>
+        <form className="form--login" onSubmit={handleLogin}>
+          <h1>Chupacabros</h1>
+          <h2>Please sign in</h2>
+          <fieldset>
+            <label htmlFor="inputEmail"> Email address </label>
+            <input
+              type="email"
+              onChange={(evt) => setUser(evt.target.value)}
+              className="form-control"
+              placeholder="Email address"
+              required
+              autoFocus
+            />
+          </fieldset>
+          <fieldset>
+            <label htmlFor="inputPassword"> Password </label>
+            <input
+              type="Password"
+              onChange={(evt) => setUser(evt.target.value)}
+              className="form-control"
+              placeholder="Password"
+              required
+              autoFocus
+            />
+          </fieldset>
+          <fieldset>
+            <button type="submit">Sign in</button>
+          </fieldset>
+        </form>
+      </section>
+      <section className="link--register">
+        <Link to="/register">Not a member yet?</Link>
+      </section>
+    </main>
+  );
+};
---
+++ src/components/nav/NavBar.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./NavBar.css";
+
+export const NavBar = () => {
+  return (
+    <ul className="navbar">
+      <li className="navbar__item active">
+        <Link className="navbar__link" to="/employees">
+          Employees
+        </Link>
+      </li>
+      <li className="navbar__item">
+        <Link className="navbar__link" to="/customers">
+          Customers
+        </Link>
+      </li>
+      <li className="navbar__item">
+        <Link className="navbar__link" to="/tickets">
+          Tickets
+        </Link>
+      </li>
+      <li className="navbar__item">
+        <Link
+          className="navbar__link"
+          to="#"
+          onClick={() => {
+            localStorage.removeItem("chupacabro_user");
+          }}
+        >
+          Logout
+        </Link>
+      </li>
+    </ul>
+  );
+};
